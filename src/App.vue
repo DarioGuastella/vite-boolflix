4ab43e0c8d425a441485a924f288d6c9ed57ba6c
DarioGuastella <jimmyguastella@gmail.com>
2023-12-04 16:28:56 +0100

    inizializza progetto

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import axios from 'axios';
+import { store } from "./store.js"
+export default {
+  components: {
+
+
+  },
+  data() {
+    return {
+      store,
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<template>
+  Prova
+</template>
+
+<style scoped></style>
---
+++ src/store.js
@@ -0,0 +1,6 @@
+import { reactive } from 'vue'
+
+export const store = reactive({
+    apiUrl: "https://api.themoviedb.org/3/search/movie?api_key=57a37e8d18ae807d791663e015455ec5&query=",
+    searchInput: ""
+});
