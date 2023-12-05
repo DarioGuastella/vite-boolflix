c8f34f999b0459afeddac74e4ccf828031ff1fd1
DarioGuastella <jimmyguastella@gmail.com>
2023-12-05 21:31:05 +0100

    feat: logo link refresh page

--- src/App.vue
+++ src/App.vue
@@ -65,9 +65,10 @@ export default {
   <header>
 
     <div class="myContainer d-flex justify-content-between">
-      <img id="logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png"
-        alt="">
+      <a class="w-100" href="."><img id="logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png"
+          alt="">
+      </a>
       <SearchTitles @search="getTitles" />
     </div>
   </header>
